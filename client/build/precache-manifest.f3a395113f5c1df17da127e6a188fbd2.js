94d05d4cf4a2ce6ab6485a14e5701a5de2d181d9
Jennifer Wong <jennifer.design@yahoo.com>
2020-10-11 19:46:27 -0500

    Ran command to generate static build files

---
+++ client/build/precache-manifest.f3a395113f5c1df17da127e6a188fbd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f679d70dc29f7b4efef4fb0805e93a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db7a580f9f43e19e7cf0",
+    "url": "/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "7c3149bbf75ec0cb6f28",
+    "url": "/static/css/main.e6847ed7.chunk.css"
+  },
+  {
+    "revision": "db7a580f9f43e19e7cf0",
+    "url": "/static/js/2.0e05293f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0e05293f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3149bbf75ec0cb6f28",
+    "url": "/static/js/main.94d4d288.chunk.js"
+  },
+  {
+    "revision": "2b8f580612459d80dc88",
+    "url": "/static/js/runtime-main.f65105ae.js"
+  }
+]);
